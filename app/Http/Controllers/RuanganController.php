073219ad7725997708310acd480fa390fdac97d7
Dewinta Afriani <dewintaafriani11@gmail.com>
2017-03-25 03:39:19 +0800

    posttest4

--- app/Http/Controllers/RuanganController.php
+++ app/Http/Controllers/RuanganController.php
@@ -9,20 +9,43 @@ use App\Ruangan;
 
 class RuanganController extends Controller
 {
-    public function awal()
-
-    {
- return "SELAMAT DATANG";
-    }
-    public function tambah()
-    {
-    	return $this->simpan();
+ public function awal()
+ {
+ return view ('ruangan.awal',['data'=>Ruangan::all()]);
 }
-public function simpan()
+public function tambah()
 {
-	$ruangan = new ruangan();
-	$ruangan->tittle = '406';
-	$ruangan->save();
-	return "data dengan ruangan {$ruangan->tittle} telah disimpan";
+	return view ('ruangan.tambah');
 }
+public function simpan(Request $input)
+{
+	$ruangan=new Ruangan();
+	$ruangan->tittle=$input->tittle;
+	$informasi=$ruangan->save()?'Berhasil simpan data' : 'Gagal simpan data';
+	return redirect('ruangan')->with(['informasi'=>$informasi]);
+}
+public function lihat($id)
+{	
+	$ruangan=Ruangan::find($id);
+	return view('ruangan.lihat')->with (array('ruangan'=>$ruangan));
+}
+public function edit($id){
+	$ruangan=Ruangan::find($id);
+	return view('ruangan.edit')->with(array('ruangan'=>$ruangan));
+}
+	
+	public function update($id, Request $input)
+	{
+    $ruangan=Ruangan::find($id);
+	$ruangan->tittle=$input->tittle;
+	$informasi=$ruangan->save()?'Berhasil update data' : 'Gagal update data';
+	return redirect('ruangan')->with(['informasi'=>$informasi]);
+	}
+	public function hapus($id)
+	{
+		$ruangan=Ruangan::find($id);
+		$informasi=$ruangan->delete()?'Berhasil hapus data' : 'Gagal hapus data';
+		return redirect('ruangan')->with(['informasi'=>$informasi]);
+	}
+
 }
---
+++ resources/views/ruangan/form.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+	<label class="col-sm-2 control-label">Username</label>
+	<div class="col-sm-10">
+		{!! Form::text('tittle',null,['class'=>'form-control','placeholder'=>"Tittle"]) !!}
+		</div>
+</div>
+
